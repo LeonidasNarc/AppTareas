4fdd73c89305cb250c6924640c2e90f47dfc615c
LeonidasNarc <narcisoleonidas97@gmail.com>
2022-03-20 17:32:34 -0400

    agregar

--- backend/routes/tareaRoutes.js
+++ backend/routes/tareaRoutes.js
@@ -3,11 +3,10 @@ const router = express.Router()
 const {getTareas, postTareas, putTareas, deleteTareas}= require('../controllers/tareasControllers')
 
 router.route('/').get(getTareas).post(postTareas)
-
-
 // router.get('/', getTareas)
 
 // router.post('/', postTareas)
+
 
 router.route('/:id').delete(deleteTareas).put(putTareas)
 
